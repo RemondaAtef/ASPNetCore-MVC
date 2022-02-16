07879e9f6068274b5cd14b71acee26bd081437cd
RemondaAtef <64480593+RemondaAtef@users.noreply.github.com>
2022-02-16 14:48:50 +0200

    first commit

---
+++ Controllers/Test1Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstProject2_GP.Controllers
+{
+    public class Test1Controller : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [Route("~/About")]
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/Test2Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstProject2_GP.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        [Route("~/Contact")]
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [Route("~/Gallery")]
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+    }
+}
